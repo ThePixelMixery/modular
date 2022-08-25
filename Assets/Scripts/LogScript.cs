4ba11e751204ac306c3601030dc67852a1c49b74
Aya <pixelmixery@gmail.com>
2022-08-26 07:25:45 +1000

    WebGL changes

--- Assets/Scripts/LogScript.cs
+++ Assets/Scripts/LogScript.cs
@@ -1,7 +1,6 @@
 using System;
 using System.Collections.Generic;
-using Firebase;
-using Firebase.Database;
+using FirebaseWebGL.Scripts.FirebaseBridge;
 using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
@@ -39,7 +38,7 @@ public class LogScript : MonoBehaviour
 
     private static TMP_InputField IDInput;
 
-    private static DatabaseReference reference;
+//    private static DatabaseReference reference;
 
     private static bool Initialised = false;
 
@@ -58,8 +57,7 @@ public class LogScript : MonoBehaviour
         if (Initialised == false){
         Initialised = true;
         // Get the root reference location of the database.
-        reference =
-            FirebaseDatabase.DefaultInstance.RootReference;
+//        reference = FirebaseDatabase.DefaultInstance.RootReference;
         Debug.Log(reference + " found");
         IDInput =
             GameObject.Find("InputField_ID").GetComponent<TMP_InputField>();
@@ -73,20 +71,18 @@ public class LogScript : MonoBehaviour
     }
 
     public static void WriteNewLogEntry(string gameEvent, string tag1, string tag2) {
-        // Create new entry at /user-scores/$userid/$scoreid and at
-        // /leaderboard/$scoreid simultaneously
-        string key = reference.Child("participants").Child(participantId).Push().Key;
-        Debug.Log(key);
         LogEntry logEntry = new LogEntry(gameEvent, tag1, tag2);
         DateTime dt = DateTime.Now;
         string logTime = dt.ToString("MM/dd");
-        logEntry.time = dt.ToString("HH:mm:ss");
-        Dictionary<string, System.Object> entryValues = logEntry.ToDictionary();
-
-        Dictionary<string, System.Object> childUpdates = new Dictionary<string, System.Object>();
-        Debug.Log(childUpdates);
-        childUpdates["/participants/" + participantId + "/" + logTime + "/" + key] = entryValues;
-
-        reference.UpdateChildrenAsync(childUpdates);
+        logEntry.time = dt.ToString("HH:mm:ss.fff");
+        string json = JsonUtility.ToJson(logEntry);
+        Firebasebridge.FirebaseDatabase
+            .PushJSON("/participants/" +
+            participantId +
+            "/", //Database path  
+            logTime +"/",
+            json, //JSON string to push to the specified path
+            "DisplayInfo",
+            "DisplayErrorObject");
     }
 }
